6955ee3d181f197776bfefb48aae464a2f9f22fd
GitHub Action <action@github.com>
2024-11-03 11:20:47 -0300

    Initialize Spring Boot project with Maven Wrapper
    Added foundational project files including `.gitignore`, `.gitattributes`, `application.properties`, and necessary Maven configurations. This setup includes dependencies for Spring Boot, JPA, Thymeleaf, and Spring Security, among others.

---
+++ src/main/java/com/github/dataprocessutils/dataprocessapi/DataProcessApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.dataprocessutils.dataprocessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProcessApiApplication.class, args);
+    }
+
+}
